59767e9e5cedfe11909b8b3e5582e347f3076704
Stormy-Scratch <shitintheface1305@gmail.com>
2019-11-29 19:31:53 +0700

    Successfully Login

--- src/test/LoginTest.java
+++ src/test/LoginTest.java
@@ -3,45 +3,72 @@ package test;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
 
+import factory.ExcelUtils;
 import factory.base;
 import factory.serviceFactory;
 import pageFactory.loginPage;
 
 import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class LoginTest extends base {
 	
-	public final String FILEPATH = "E:\\testDDT\\FptVN.xlsx";
-	public final String SHEET = "LoginTest";
+	//Creating Excel path and Sheet
+	public final String FILEPATH = "E:\\Eclipse-Projects\\HocTrucTuyen-Testing\\dataTestCase_Login.xlsx";
+	public final String SHEET = "Login";
 	
+	
+	//Rows and Collumns
+	public final int ROW = 8;
+	public final int COL = 6;
+  	public final int USERNAME_COL =3;
+	public final int PASSWORD_COL =4;
+	public final int EXPECTED_COL = 6;
+	public final int SUCCESS_DATA_ROW=2;
+	
+	//POM related
 	loginPage objLogin;
 	serviceFactory objService;
 	
 	//URL
+	public final String homeURL = "https://hoctructuyen.vanlanguni.edu.vn";
+	public final String loginURL = "https://hoctructuyen.vanlanguni.edu.vn/login/index.php";
 	
+	//Login data instance
 	public String username;
 	public String password;
 	
 	@Test(priority = 1)
-  public void login() {
+  public void login() throws Exception {
+		//POM applied
 		objLogin = new loginPage(driver);
+		objService = new serviceFactory(driver);
 		
-		String user = "t177225";
-		String password = "t177225";
-		driver.findElement(By.xpath("//a[@href='https://hoctructuyen.vanlanguni.edu.vn/login/index.php']")).click();
-	  //Insert Username and Password
-	  driver.findElement(By.xpath("//input[@id='username']")).sendKeys(user);
-	  
-	  driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
-	  
-	  driver.findElement(By.xpath("//input[@id='loginbtn']")).click();
-	  
-	  String actualResult = driver.findElement(By.xpath("//a[@title='View profile']")).getText();
-	  String expectedResult = "T177225 - NGUYỄN VĨNH TRí - K23T01";
-	  Assert.assertEquals(actualResult, expectedResult);
-	  
-	  driver.findElement(By.xpath("//a[text()='Log out']")).click();
+		ExcelUtils.setExcelFile(FILEPATH, SHEET);
+		ExcelUtils.setrowcolumn(ROW, COL);
+		
+		objService.waitForElementToClickable(objLogin.getLoginLink());
+		objLogin.loginLinkClick();
+		
+		objService.waitForElementToClickable(objLogin.getLoginBtn());
+		
+		username = ExcelUtils.getCellData(SUCCESS_DATA_ROW, USERNAME_COL);
+		password = ExcelUtils.getCellData(SUCCESS_DATA_ROW, PASSWORD_COL);
+		
+		objLogin.login(username, password);
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		Thread.sleep(3000);
+		
+		if(driver.getCurrentUrl().equals(homeURL)) {
+			Assert.assertEquals(objLogin.getProfileText(), ExcelUtils.getCellData(SUCCESS_DATA_ROW, EXPECTED_COL));
+			objLogin.logoutClick();
+		}
+		
   }
 	
 //	@Test(priority = 1)
